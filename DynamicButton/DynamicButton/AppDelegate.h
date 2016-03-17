1e93c2da81db2dba36f8be4110a07a2c735eb46e
boyancao <boyancao@macdeMacBook-Pro.local>
2016-03-17 17:58:14 +0800

    first commit

---
+++ DynamicButton/DynamicButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicButton
+//
+//  Created by boyancao on 16/3/17.
+//  Copyright © 2016年 boyancao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DynamicButton/DynamicButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicButton
+//
+//  Created by boyancao on 16/3/17.
+//  Copyright © 2016年 boyancao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
